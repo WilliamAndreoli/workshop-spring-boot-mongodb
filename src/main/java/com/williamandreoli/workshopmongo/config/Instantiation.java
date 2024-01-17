ab7b3f9090fb392eb231378d15c6917d94c5188f
William Andreoli <willandreoli33@gmail.com>
2024-01-17 16:12:35 -0300

    Adding comments to posts

--- src/main/java/com/williamandreoli/workshopmongo/config/Instantiation.java
+++ src/main/java/com/williamandreoli/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.williamandreoli.workshopmongo.domain.Post;
 import com.williamandreoli.workshopmongo.domain.User;
 import com.williamandreoli.workshopmongo.dto.AuthorDTO;
+import com.williamandreoli.workshopmongo.dto.CommentDTO;
 import com.williamandreoli.workshopmongo.repository.PostRepository;
 import com.williamandreoli.workshopmongo.repository.UserRepository;
 
@@ -26,7 +27,7 @@ public class Instantiation implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		SimpleDateFormat idf = new SimpleDateFormat("dd/MM/yyyy");
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		userRepository.deleteAll();
 		postRepository.deleteAll();
@@ -37,13 +38,22 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, (Date) idf.parse("21/03/2018"), "Partiu Viajar", "Vou viajar para São Paulo, Abraços!", new AuthorDTO(maria));
-		Post post2 = new Post(null, (Date) idf.parse("05/06/2022"), "Bom Dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		Post post1 = new Post(null, (Date) sdf.parse("21/03/2018"), "Partiu Viajar", "Vou viajar para São Paulo, Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, (Date) sdf.parse("05/06/2022"), "Bom Dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa Viajem mano", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("21/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("boa Viajem mano", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
-		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		maria.getPosts().addAll(Arrays.asList(post1, post2));	
 		userRepository.save(maria);
+		
+		
 		
 	}
 	
--- src/main/java/com/williamandreoli/workshopmongo/domain/Post.java
+++ src/main/java/com/williamandreoli/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.williamandreoli.workshopmongo.domain;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.williamandreoli.workshopmongo.dto.AuthorDTO;
+import com.williamandreoli.workshopmongo.dto.CommentDTO;
 
 @Document(collection="post")
 public class Post {
@@ -17,6 +20,9 @@ public class Post {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+
+	private List<CommentDTO> comments = new ArrayList<>();
+	
 	
 	public Post() {
 	}
@@ -68,6 +74,15 @@ public class Post {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+	
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
