8ca60f1e0e225a6f95cbbfefeb6199c49b525c13
William Andreoli <willandreoli33@gmail.com>
2024-01-17 15:22:59 -0300

    Author data projection with DTO

--- src/main/java/com/williamandreoli/workshopmongo/config/Instantiation.java
+++ src/main/java/com/williamandreoli/workshopmongo/config/Instantiation.java
@@ -1,8 +1,8 @@
 package com.williamandreoli.workshopmongo.config;
 
-import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.williamandreoli.workshopmongo.domain.Post;
 import com.williamandreoli.workshopmongo.domain.User;
+import com.williamandreoli.workshopmongo.dto.AuthorDTO;
 import com.williamandreoli.workshopmongo.repository.PostRepository;
 import com.williamandreoli.workshopmongo.repository.UserRepository;
 
@@ -34,8 +35,10 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, (Date) idf.parse("21/03/2018"), "Partiu Viajar", "Vou viajar para São Paulo, Abraços!", maria);
-		Post post2 = new Post(null, (Date) idf.parse("05/06/2022"), "Partiu Viajar", "Vou viajar para São Paulo, Abraços!", maria);
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, (Date) idf.parse("21/03/2018"), "Partiu Viajar", "Vou viajar para São Paulo, Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, (Date) idf.parse("05/06/2022"), "Bom Dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		postRepository.saveAll(Arrays.asList(post1, post2));
